3f5024ce49a3bbe84851a11bcb509d87015103f1
Julyo Hidalgo <julyohidalgo0@gmail.com>
2023-03-02 16:12:16 -0300

    adicionando DAO, e config

--- DAO/DAO.php
+++ DAO/DAO.php
@@ -1,1 +1,9 @@
 <?php
+
+use \PDO;
+
+abstract class DAO extends PDO{
+    protected $conexao;
+
+     //$dns = "mysql:host=" . $_ENV['db']['host'] . ";dbname=" .$_ENV['db']['database'];
+} 
--- index.php
+++ index.php
@@ -3,11 +3,3 @@
 include 'config.php';
 include 'autoload.php';
 include 'rotas.php';
-
-/*$cidade = ["Jaú", "Bauru", "Dois córregos"];
-
-//var_dump($cidade);
-
-include 'Controller/Controller.php';
-
-\ApiCEP\Controller\Controller::getResponseAsJSON($cidade);*/
